049ed6a30ef4f409d7aa40ccfd0d04991db3d36f
Rid <grant@seednet.eu>
2019-02-05 11:42:09 +0000

    Updates

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,17 @@
 FROM gitlab/gitlab-ce:latest
 
+RUN groupmod -g 9999 nogroup
+RUN usermod -g 9999 nobody
+RUN usermod -u 9999 nobody
+RUN usermod -g 9999 sshd
+RUN usermod -g 9999 sync
+
 ADD scripts /scripts
 RUN chmod -R +x /scripts
 
-ENTRYPOINT ["/scripts/entrypoint.sh"]
-
 CMD ["/assets/wrapper"]
+
+ENTRYPOINT ["/scripts/entrypoint.sh"]
 
 HEALTHCHECK --interval=60s --timeout=30s --retries=5 \
 CMD /opt/gitlab/bin/gitlab-healthcheck --fail --max-time 10
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 cat << EOF > /etc/gitlab/gitlab.rb
 external_url 'https://${DOMAIN}'
@@ -14,12 +15,26 @@ nginx['proxy_set_headers'] = {
 
 }
 gitlab_rails['gitlab_shell_ssh_port'] = ${SSH_PORT}
-unicorn['worker_processes'] = 1
-sidekiq['concurrency'] = 1
+unicorn['worker_processes'] = 2
+sidekiq['concurrency'] = 2
+unicorn['worker_timeout'] = 180
 nginx['real_ip_header'] = 'X-Real-IP'
 nginx['set_real_ip_from'] = '172.20.0.0/12'
 postgresql['shared_buffers'] = "1MB"
 prometheus_monitoring['enable'] = false
 EOF
+
+if [[ ! $(cat /assets/wrapper | grep 'Cylo') ]]; then
+sed '/tail/a \
+\
+echo "Calling back to Cylo API..." \
+if [ ! -f /etc/app_configured ]; then \
+    touch /etc/app_configured \
+    until [[ $(curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST "https://api.cylo.io/v1/apps/installed/${INSTANCE_ID}" | grep "200") ]] \
+        do \
+        sleep 5 \
+    done \
+fi' /assets/wrapper
+fi
 
 exec "$@"
